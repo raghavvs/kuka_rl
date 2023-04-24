1290ac3b385b08a4087ba08ad81eca5f8f80b617
Raghav Navaratna <rsnavaratna@gmail.com>
2023-04-24 14:35:57 -0500

    cleanup

--- main_a2c.py
+++ main_a2c.py
@@ -1,27 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Kuka
-# 
-# ---
-# 
-# You are welcome to use this coding environment to train your agent for the project.  Follow the instructions below to get started!
-# 
-# ### Start the Environment
-# 
-
-# Make sure that you're in the right virtual environment and the right python version.
-
-# In[1]:
-
-
 """ get_ipython().system('python --version')
 get_ipython().system('pip install pybullet')
 get_ipython().system('pip install tensorboardX') """
-
-
-# In[2]:
-
 
 import matplotlib.pyplot as plt
 import sys
@@ -32,7 +14,14 @@ from copy import deepcopy
 import numpy as np
 import random
 from PIL import Image
-from torch.utils.tensorboard import SummaryWriter
+from tensorboardX import SummaryWriter
+
+import functools
+import multiprocessing as mp
+from multiprocessing import Pipe
+from multiprocessing import Process
+import signal
+import warnings
 
 import torch
 import torch.nn as nn
@@ -40,22 +29,125 @@ import torch.nn.functional as F
 import torchvision.transforms as T
 import torch.optim as optim
 
-from pybullet_envs.bullet.kuka_diverse_object_gym_env import KukaDiverseObjectEnv
 from gym import spaces
+from pybullet_envs.bullet.kuka_diverse_object_gym_env import KukaDiverseObjectEnv  
 import pybullet as p
 
-env = KukaDiverseObjectEnv(renders=False, isDiscrete=False, removeHeightHack=False, maxSteps=20)
-env.cid = p.connect(p.DIRECT)
-action_space = spaces.Box(low=-1, high=1, shape=(5,1))
+resize = T.Compose([T.ToPILImage(),
+                    T.Resize(40, interpolation=Image.CUBIC),
+                    T.ToTensor()])
+def worker(remote, env_fn):
+    # Ignore CTRL+C in the worker process
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+    env = env_fn()
+    try:
+        while True:
+            cmd, data = remote.recv()
+            if cmd == 'step':
+                ob, reward, done, info = env.step(data)
+                remote.send((ob, reward, done, info))
+            elif cmd == 'get_screen':
+                screen = env._get_observation()
+                screen = env._get_observation().transpose((2, 0, 1))
+                screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+                screen = torch.from_numpy(screen)
+                screen = resize(screen).unsqueeze(0)
+                remote.send(screen)
+            elif cmd == 'reset':
+                ob = env.reset()
+                remote.send(ob)
+            elif cmd == 'close':
+                remote.close()
+                break
+            elif cmd == 'get_spaces':
+                remote.send((env.action_space, env.observation_space))
+            else:
+                raise NotImplementedError
+    finally:
+        env.close()
+
+resize = T.Compose([T.ToPILImage(),
+                    T.Resize(40, interpolation=Image.CUBIC),
+                    T.ToTensor()])
+class MultiprocessVectorEnv:
+    def __init__(self, env_fns):
+        nenvs = len(env_fns)
+        self.remotes, self.work_remotes = zip(*[Pipe() for _ in range(nenvs)])
+        self.ps = \
+            [Process(target=worker, args=(work_remote, env_fn))
+             for (work_remote, env_fn) in zip(self.work_remotes, env_fns)]
+        for p in self.ps:
+            p.start()
+        self.last_obs = [None] * self.num_envs
+        self.remotes[0].send(('get_spaces', None))
+        self.action_space, self.observation_space = self.remotes[0].recv()
+        self.closed = False
+
+    def __del__(self):
+        if not self.closed:
+            self.close()
+
+
+    def step(self, actions):
+        self._assert_not_closed()
+        for remote, action in zip(self.remotes, actions):
+            remote.send(('step', action))
+        results = [remote.recv() for remote in self.remotes]
+        self.last_obs, rews, dones, infos = zip(*results)
+        return self.last_obs, rews, dones, infos
+    
+    def get_screen(self):
+        for remote in self.remotes:
+            remote.send(('get_screen', None))
+        results = [remote.recv() for remote in self.remotes]
+        screens = torch.cat(results,dim=0)
+        return screens
+
+    def reset(self, mask=None):
+        self._assert_not_closed()
+        if mask is None:
+            mask = np.zeros(self.num_envs)
+        for m, remote in zip(mask, self.remotes):
+            if not m:
+                remote.send(('reset', None))
+
+        obs = [remote.recv() if not m else o for m, remote,
+               o in zip(mask, self.remotes, self.last_obs)]
+        self.last_obs = obs
+        return obs
+
+    def close(self):
+        self._assert_not_closed()
+        self.closed = True
+        for remote in self.remotes:
+            remote.send(('close', None))
+        for p in self.ps:
+            p.join()
+
+    @property
+    def num_envs(self):
+        return len(self.remotes)
+
+    def _assert_not_closed(self):
+        assert not self.closed, "This env is already closed"
+
+def make_env(idx, test):
+    env = KukaDiverseObjectEnv(renders=False, isDiscrete=False, removeHeightHack=False, maxSteps=20)
+    env.observation_space = spaces.Box(low=0., high=1., shape=(84, 84, 3), dtype=np.float32)
+    env.action_space = spaces.Box(low=-1, high=1, shape=(5,1))
+    return env
+
+def make_batch_env(test):
+    return MultiprocessVectorEnv(
+        [functools.partial(make_env, idx, test)
+            for idx in range(mp.cpu_count()*2)])
+
+envs = make_batch_env(test=False)
 
 # if gpu is to be used
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
 # Actor-Critic implementation
-
-# In[3]:
-
 
 import torch
 import torch.nn as nn
@@ -193,57 +285,37 @@ class ActorCritic(nn.Module):
 
 # Examine the state and action spaces.
 
-# In[4]:
-
-
-resize = T.Compose([T.ToPILImage(),
-                    T.Resize(40, interpolation=Image.CUBIC),
-                    T.ToTensor()])
-
-def get_screen():
-    # Returned screen requested by gym is 400x600x3, but is sometimes larger
-    # such as 800x1200x3. Transpose it into torch order (CHW).
-    #env.render(mode='human')
-    screen = env._get_observation().transpose((2, 0, 1))
-    # Convert to float, rescale, convert to torch tensor
-    # (this doesn't require a copy)
-    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
-    screen = torch.from_numpy(screen)
-    # Resize, and add a batch dimension (BCHW)
-    return resize(screen).unsqueeze(0).to(device)
-
-
-env.reset()
+envs.reset()
 
 # number of agents
-num_agents = 1
+num_agents = envs.num_envs
 print('Number of agents:', num_agents)
 
-init_screen = get_screen()
+init_screen = envs.get_screen().to(device)
 _, _, screen_height, screen_width = init_screen.shape
 
 # size of each action
-action_size = env.action_space.shape[0]
+action_size = envs.action_space.shape[0]
 print('Size of each action:', action_size)
 
 plt.figure()
-plt.imshow(init_screen.cpu().squeeze(0).permute(1, 2, 0).numpy(),
+plt.imshow(init_screen[0].cpu().squeeze(0).permute(1, 2, 0).numpy(),
            interpolation='none')
 plt.title('Example extracted screen')
 plt.show()
 
-
-# In[5]:
-
-
 writer = SummaryWriter()
 i_episode = 0
-ten_rewards = 0
 def collect_trajectories(envs, policy, tmax=200, nrand=5):
-
+    
     global i_episode 
-    global ten_rewards
     global writer
+    
+    episode_rewards = 0
+    
+    
+    def to_tensor(x, dtype=np.float32):
+        return torch.from_numpy(np.array(x).astype(dtype)).to(device)
     
     #initialize returning lists and start the game!
     state_list=[]
@@ -257,14 +329,14 @@ def collect_trajectories(envs, policy, tmax=200, nrand=5):
 
     # perform nrand random steps
     for _ in range(nrand):
-        action = np.random.randn(action_size)
+        action = np.random.randn(num_agents, action_size)
         action = np.clip(action, -1.0, 1.0)
-        _, reward, done, _  = envs.step(action)
-        reward = torch.tensor([reward], device=device)
+        _, reward, done, _   = envs.step(action)
+        reward = torch.tensor(reward, device=device)
         
 
     for t in range(tmax):
-        states = get_screen()
+        states = envs.get_screen().to(device)
         action_est, values = policy(states)
         sigma = nn.Parameter(torch.zeros(action_size))
         dist = torch.distributions.Normal(action_est, F.softplus(sigma).to(device))
@@ -275,9 +347,9 @@ def collect_trajectories(envs, policy, tmax=200, nrand=5):
         actions = actions.detach()
         
         env_actions = actions.cpu().numpy()
-        _, reward, done, _  = envs.step(env_actions[0])
-        rewards = torch.tensor([reward], device=device)
-        dones = torch.tensor([done], device=device)
+        _, reward, done, _  = envs.step(env_actions)
+        rewards = to_tensor(reward)
+        dones = to_tensor(done)
 
         state_list.append(states.unsqueeze(0))
         prob_list.append(log_probs.unsqueeze(0))
@@ -287,13 +359,12 @@ def collect_trajectories(envs, policy, tmax=200, nrand=5):
         done_list.append(dones)
 
         if np.any(dones.cpu().numpy()):
-            ten_rewards += reward
-            i_episode += 1
+            episode_rewards += rewards.sum(dim=0)
+            i_episode += dones.sum(dim=0)
+            writer.add_scalar('Episodes average rewards', episode_rewards.item()/dones.sum(dim=0).item(), i_episode.item())
             state = envs.reset()
-            if i_episode%10 == 0:
-                writer.add_scalar('ten episodes average rewards', ten_rewards/10.0, i_episode)
-                ten_rewards = 0
-
+            episode_rewards = 0
+                
     state_list = torch.cat(state_list, dim=0)
     prob_list = torch.cat(prob_list, dim=0)
     action_list = torch.cat(action_list, dim=0)
@@ -301,10 +372,6 @@ def collect_trajectories(envs, policy, tmax=200, nrand=5):
     value_list = torch.cat(value_list, dim=0)
     done_list = torch.cat(done_list, dim=0)
     return prob_list, state_list, action_list, reward_list, value_list, done_list
-
-
-# In[6]:
-
 
 def calc_returns(rewards, values, dones):
     n_step = len(rewards)
@@ -339,10 +406,17 @@ def calc_returns(rewards, values, dones):
 
     return GAE, returns
 
-
-# In[7]:
-
-
+def get_screen():
+    # Returned screen requested by gym is 400x600x3, but is sometimes larger
+    # such as 800x1200x3. Transpose it into torch order (CHW).
+    #env.render(mode='human')
+    screen = env._get_observation().transpose((2, 0, 1))
+    # Convert to float, rescale, convert to torch tensor
+    # (this doesn't require a copy)
+    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+    screen = torch.from_numpy(screen)
+    # Resize, and add a batch dimension (BCHW)
+    return resize(screen).unsqueeze(0).to(device)
 def eval_policy(envs, policy, tmax=1000):
     reward_list=[]
     state = envs.reset()
@@ -361,43 +435,6 @@ def eval_policy(envs, policy, tmax=1000):
             break
     return reward_list
 
-
-# ## Network Architecture
-# An actor-critic structure with continuous action space is used for this project. The policy consists of 3 parts, a shared hidden layers, actor, and critic.
-# The actor layer outputs the mean value of a normal distribution, from which the agent's action is sampled. The critic layer yields the value function.
-# 
-# - Shared layer:
-# ```
-# Input State(48,48,3) -> Conv2d(3, 16, 5, 2) -> BatchNorm2d(16) -> Conv2d(16, 32, 5, 2)-> BatchNorm2d(32)
-# -> Conv2d(32, 32, 5, 2) -> BatchNorm2d(32) -> Dense(128) -> LeakyReLU -> Dense(128) -> LeakyReLU -> Dense(64) -> LeakyReLU
-# ```
-# - Actor and Critic layers:
-# ```
-# LeakyRelu -> Dense(64) -> LeakyRelu -> Dense(4)-> tanh -> Actor's output
-# LeakyReLU -> Dense(64) -> LeakyRelu -> Dense(1) -> Critic's output
-# ```
-# 
-# ### Model update using PPO/GAE
-# The hyperparameters used during training are:
-# 
-# Parameter | Value | Description
-# ------------ | ------------- | -------------
-# Number of Agents | 1 | Number of agents trained simultaneously
-# tmax | 20 | Maximum number of steps per episode
-# Epochs | 10 | Number of training epoch per batch sampling
-# Batch size | 128 | Size of batch taken from the accumulated  trajectories
-# Discount (gamma) | 0.993 | Discount rate 
-# Epsilon | 0.07 | Ratio used to clip r = new_probs/old_probs during training
-# Gradient clip | 10.0 | Maximum gradient norm 
-# Beta | 0.01 | Entropy coefficient 
-# Tau | 0.95 | tau coefficient in GAE
-# Learning rate | 2e-4 | Learning rate 
-# Optimizer | Adam | Optimization method
-# 
-
-# In[8]:
-
-
 # run your own policy!
 policy=ActorCritic(state_size=(screen_height, screen_width),
               action_size=action_size,
@@ -411,17 +448,8 @@ policy=ActorCritic(state_size=(screen_height, screen_width),
 # optim.SGD is also possible
 optimizer = optim.Adam(policy.parameters(), lr=2e-4)
 
-
-# In[9]:
-
-
 PATH = 'policy_ppo.pt'
 
-
-# In[9]:
-
-
-writer = SummaryWriter()
 best_mean_reward = None
 
 scores_window = deque(maxlen=100)  # last 100 scores
@@ -430,22 +458,22 @@ discount = 0.993
 epsilon = 0.07
 beta = .01
 opt_epoch = 10
-episode = 100
+season = 1000000
 batch_size = 128
-tmax = 1000 #env episode steps
+tmax = 1000//num_agents#env episode steps
 save_scores = []
 start_time = timeit.default_timer()
 
-for s in range(episode):
+for s in range(season):
     policy.eval()
-    old_probs_lst, states_lst, actions_lst, rewards_lst, values_lst, dones_list = collect_trajectories(envs=env,
+    old_probs_lst, states_lst, actions_lst, rewards_lst, values_lst, dones_list = collect_trajectories(envs=envs,
                                                                                                        policy=policy,
                                                                                                        tmax=tmax,
                                                                                                        nrand = 5)
 
-    episode_score = rewards_lst.sum(dim=0).item()
-    scores_window.append(episode_score)
-    save_scores.append(episode_score)
+    season_score = rewards_lst.sum(dim=0).sum().item()
+    scores_window.append(season_score)
+    save_scores.append(season_score)
     
     gea, target_value = calc_returns(rewards = rewards_lst,
                                      values = values_lst,
@@ -472,7 +500,7 @@ for s in range(episode):
     values_lst = concat_all(values_lst)
     gea = concat_all(gea)
     target_value = concat_all(target_value)
-
+    
     # gradient ascent step
     n_sample = len(old_probs_lst)//batch_size
     idx = np.arange(len(old_probs_lst))
@@ -521,6 +549,7 @@ for s in range(episode):
     mean_reward = np.mean(scores_window)
     writer.add_scalar("epsilon", epsilon, s)
     writer.add_scalar("beta", beta, s)
+    writer.add_scalar("Score", mean_reward, s)
     # display some progress every n iterations
     if best_mean_reward is None or best_mean_reward < mean_reward:
                 # For saving the model and possibly resuming training
@@ -534,7 +563,7 @@ for s in range(episode):
                     print("Best mean reward updated %.3f -> %.3f, model saved" % (best_mean_reward, mean_reward))
                 best_mean_reward = mean_reward
     if s>=25 and mean_reward>50:
-        print('Environment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(s+1, mean_reward))
+        print('Environment solved in {:d} seasons!\tAverage Score: {:.2f}'.format(s+1, mean_reward))
         break
 
 
@@ -542,24 +571,16 @@ print('Average Score: {:.2f}'.format(mean_reward))
 elapsed = timeit.default_timer() - start_time
 print("Elapsed time: {}".format(timedelta(seconds=elapsed)))
 writer.close()
-env.close()
-
-
-# In[12]:
-
+envs.close()
 
 fig = plt.figure()
 plt.plot(np.arange(len(save_scores)), save_scores)
 plt.ylabel('Score')
-plt.xlabel('episode #')
+plt.xlabel('Season #')
 plt.grid()
 plt.show()
 
-
-# ## Evaluation
-
-# In[10]:
-
+# Evaluation
 
 episode = 10
 scores_window = deque(maxlen=100)  # last 100 scores
@@ -574,4 +595,3 @@ for e in range(episode):
     rewards = eval_policy(envs=env, policy=policy)
     reward = np.sum(rewards,0)
     print("Episode: {0:d}, reward: {1}".format(e+1, reward), end="\n")
-
